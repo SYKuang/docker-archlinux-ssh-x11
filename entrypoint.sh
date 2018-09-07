7df685a3e5901fe19652c67ace5f84de68a748c0
Ken Kuang <sykuang.tw@gmail.com>
2018-09-07 16:28:29 +0800

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f /etc/ssh/ssh_host_rsa_key] ; then
+if [ ! -f /etc/ssh/ssh_host_rsa_key ] ; then
 # Generate ssh key
     ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key
     ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key
