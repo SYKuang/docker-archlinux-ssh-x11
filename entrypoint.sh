7c8c8528185a896cf5499843ca33421d82dcf13a
Ken Kuang <sykuang.tw@gmail.com>
2018-09-07 17:16:55 +0800

    Add ed25519 key

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ if [ ! -f /etc/ssh/ssh_host_rsa_key ] ; then
     ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -q -N ""
     ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -q -N ""
     ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -q -N ""
+    ssh-keygen -t dsa -f /etc/ssh/ssh_host_ed25519_key -q -N ""
 fi
 /bin/sshd -D
